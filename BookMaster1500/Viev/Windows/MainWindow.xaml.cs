74ae229fd8f2ca3d949a4bb6666a471baab54144
Гоголев.Валерий <.@TERM1>
2025-01-22 16:29:56 +0300

    Меню немного

--- BookMaster1500/Viev/Windows/MainWindow.xaml.cs
+++ BookMaster1500/Viev/Windows/MainWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace BookMaster1500.Viev.Windows
 {
@@ -23,5 +11,55 @@ namespace BookMaster1500.Viev.Windows
         {
             InitializeComponent();
         }
+
+        private void LoginMi_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+        }
+
+        private void CloseMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void BrowseKatalogMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ManageCustomersMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CirculatoinMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReportsMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void LogoutMi_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Способ навигации в WPF
+        #region Оконная
+        // 1. Оконная осуществляет открытие нового экхемпляра окна их другого окна или страницы
+        // Алгоритм для реализации (Окно называется TestWindow.xaml)
+        // - создать экземпляр окна TestWindow
+        // - TestWindow testWindow = new TestWindow()
+        // у экземпляра окна вызывается метод Show(). Данный метод открывает модальное окно (данное действие посзовлит пользоваться предыдущем окном). ShowDialog открывает диалоговое окно (нельзя взаимодействовать с предыдущем окном)
+        // TestWindow.Show();
+        #endregion
+        // 2. Страничная
+
     }
 }
