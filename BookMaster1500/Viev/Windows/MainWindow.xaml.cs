43561597ed0cddd63ab98a94a0f8efbd1779faa7
Гоголев.Валерий <.@TERM1>
2025-01-23 09:45:35 +0300

    Со страницами поработали

--- BookMaster1500/Viev/Windows/MainWindow.xaml.cs
+++ BookMaster1500/Viev/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using BookMaster1500.Viev.Pages;
+using System.Windows;
 
 namespace BookMaster1500.Viev.Windows
 {
@@ -10,6 +11,8 @@ namespace BookMaster1500.Viev.Windows
         public MainWindow()
         {
             InitializeComponent();
+
+            LogoutMi.Visibility = Visibility.Collapsed;
         }
 
         private void LoginMi_Click(object sender, RoutedEventArgs e)
@@ -20,28 +23,28 @@ namespace BookMaster1500.Viev.Windows
 
         private void CloseMi_Click(object sender, RoutedEventArgs e)
         {
-
+            Close();
         }
 
 
         private void BrowseKatalogMi_Click(object sender, RoutedEventArgs e)
         {
-
+            MaifFrm.Navigate(new BrowseCatalogePage());
         }
 
         private void ManageCustomersMi_Click(object sender, RoutedEventArgs e)
         {
-
+            MaifFrm.Navigate(new ManageCurstomerPage());
         }
 
         private void CirculatoinMi_Click(object sender, RoutedEventArgs e)
         {
-
+            MaifFrm.Navigate(new CirculationPage());
         }
 
         private void ReportsMi_Click(object sender, RoutedEventArgs e)
         {
-
+            MaifFrm.Navigate(new ReportsPage());
         }
 
 
@@ -59,7 +62,14 @@ namespace BookMaster1500.Viev.Windows
         // у экземпляра окна вызывается метод Show(). Данный метод открывает модальное окно (данное действие посзовлит пользоваться предыдущем окном). ShowDialog открывает диалоговое окно (нельзя взаимодействовать с предыдущем окном)
         // TestWindow.Show();
         #endregion
+        #region
         // 2. Страничная
-
+        // Осуществляет открытие страниц внутри элемента Fraim
+        // Алгоритм для реализаии 
+        // - определить место для элемента Fraim (данный элемент принимает и отображает страницу)
+        // - дать имя фрейму в XAML коде
+        // - для навигации обратиться к файлу вызвать метод Navigate и передать в кчестве значения экземпляр страницы
+        // MainFrm.Navigate(new TestPage());
+        #endregion
     }
 }
