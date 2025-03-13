ff2182b0c2f3cfc013c3e9265cc61af5c2e2c8af
Гоголев.Валерий <.@TERM1>
2025-03-13 15:22:15 +0300

    Поиск реалезован

--- BookMaster1500/Viev/Pages/BrowseCatalogePage.xaml.cs
+++ BookMaster1500/Viev/Pages/BrowseCatalogePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using BookMaster1500.Model;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Controls;
 
 namespace BookMaster1500.Viev.Pages
@@ -8,11 +10,18 @@ namespace BookMaster1500.Viev.Pages
     /// </summary>
     public partial class BrowseCatalogePage : Page
     {
+        List<BookAuthor> _bookAuthors = App.context.BookAuthor.ToList();
         public BrowseCatalogePage()
         {
             InitializeComponent();
+            // Загрузка данных из таблицы в список ListView
+            BookAuthorLv.ItemsSource = _bookAuthors;
+        }
 
-            BookAuthorLv.ItemsSource = App.context.BookAuthor.ToList();
+        private void SearchBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //Алгоритм поиска
+            BookAuthorLv.ItemsSource = _bookAuthors.Where(bookAuthors => bookAuthors.Book.Title.ToLower().Contains(SearchByBookTitleTb.Text.ToLower()) && bookAuthors.Author.Name.ToLower().Contains(SearchByAuthorNameTb.Text.ToLower()));
         }
     }
 }
