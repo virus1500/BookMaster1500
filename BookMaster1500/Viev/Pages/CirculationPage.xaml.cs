f5b030886c0af60d38c2b571762f443c94613585
Гоголев.Валерий <.@TERM1>
2025-06-04 16:28:30 +0300

    ПоПЫтка

--- BookMaster1500/Viev/Pages/CirculationPage.xaml.cs
+++ BookMaster1500/Viev/Pages/CirculationPage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
+﻿using BookMaster1500.AppData;
+using BookMaster1500.Model;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BookMaster1500.Viev.Pages
 {
@@ -20,9 +12,29 @@ namespace BookMaster1500.Viev.Pages
     /// </summary>
     public partial class CirculationPage : Page
     {
+        PagenationService<Circulation> _circulationPagenation;
+        List<Circulation> _circulations = App.context.Circulation.ToList();
         public CirculationPage()
         {
             InitializeComponent();
+            CurculatuonLv.ItemsSource = _circulations;
+        }
+
+        private void CirculationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            List<Circulation> searchResults = _circulations.Where(circulation => circulation.CustomerId.ToLower().Contains(ClientIDTb.Text.ToLower())).ToList();
+
+            _circulationPagenation = new PagenationService<Circulation>(searchResults);
+
+            CurculatuonLv.ItemsSource = _circulationPagenation.CurrentPageOfItems;
+            HistoryCustomerLv.ItemsSource = _circulationPagenation.CurrentPageOfItems;
+        }
+
+        private void CurculatuonLv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Circulation selectedBook = ClientIDTb. as Circulation;
+
+            //LeftGrid.DataContext = selectedBook;
         }
     }
 }
